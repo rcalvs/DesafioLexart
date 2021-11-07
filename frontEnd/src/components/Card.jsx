d1724d75daab7108c39b900dbf5658e40fa8d1a5
rcalvs <rafaelcalvette@gmail.com>
2021-11-07 14:18:34 -0300

    Style: FrontEnd: Yellow Color for MercadoLivre Search

--- frontEnd/src/components/Card.jsx
+++ frontEnd/src/components/Card.jsx
@@ -37,7 +37,7 @@ function Card(props) {
 
   return (
     <div className='flex border-1 justify-center pt-8'>
-      <div className='container w-36 h-36 bg-blue-400 mx-2'>
+      <div className='container w-36 h-36 bg-yellow-400 mx-2'>
         <img 
           className='w-36 h-36'
           src={thumbnail}
@@ -49,12 +49,12 @@ function Card(props) {
         <h1 className='text-2xl'>{title}</h1>
         {/* <p className='italic pt-2'>
           Lorem ipsum dolor sit amet, consectetru adipiscigin elit sed</p> */}
-        <h3 className='absolute bottom-4 text-blue-500 right-8'>R$ {price}</h3>
+        <h3 className='absolute bottom-4 text-yellow-500 right-8'>R$ {price}</h3>
       </div>
 
       <div className='m-auto border-1 mx-4'>
         <a href={permalink}>
-          <button className='rounded-lg p-2 px-4 bg-blue-400 hover:bg-blue-500'>
+          <button className='rounded-lg p-2 px-4 bg-yellow-400 hover:bg-yellow-500'>
             Buy now
           </button>
         </a>
